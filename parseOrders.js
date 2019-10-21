7aef4cbd1b604cce30f3431de70238bd5cb66787
Claudius Link <claudius.link@gmail.com>
2019-10-22 00:15:39 +0200

    Simplified by using getElement*

--- parseOrders.js
+++ parseOrders.js
@@ -1,21 +1,12 @@
 var doc = window.document
 
-var oderXPath = ".//div[@id='ordersContainer']/div[@class='a-box-group a-spacing-base order']"
-// CSS Selector: div.a-box-group:nth-child(5)
-// CSS Path: ... div#ordersContainer div.a-box-group.a-spacing-base.order
-// XPath: /html/body/div[1]/div[1]/div[1]/div[5]/div[4]
+var container = doc.getElementById( "ordersContainer")
 
-var orders = doc.evaluate( oderXPath, doc.body, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null );
+var orders = container.getElementsByClassName( "order")
+console.log( orders.length )
 
-var c = 0;
-
-var order = orders.iterateNext();
-
-
-while( order ) {
-	//order.style.border = "medium dotted red"; 
-        c = c + 1;
-        order = orders.iterateNext();
+for(var i = 0; i < orders.length; i++)
+{
+   orders.item(i).style.border = "medium dotted red";
 }
 
-console.log( c )
