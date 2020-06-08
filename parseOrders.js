8fe08d8bfb824562b335ad44efa77d370c96bff6
Claudius Link <claudius.link@gmail.com>
2020-06-08 22:21:40 +0200

    Yielding and pathes

--- parseOrders.js
+++ parseOrders.js
@@ -2,10 +2,13 @@ var doc = window.document
 var container = doc.getElementById( "ordersContainer")
 var orders = container.getElementsByClassName( "order")
 console.log( orders.length )
-var o = orders.item(i)
-for(var i = 0; i < orders.length; i++)
+
+for(var o of orders)
 {
-   orders.item(i).style.border = "medium dotted red";
+  o.style.border = "medium dotted red";
+  var i = o.getElementsByClassName( "order-info" );
+  i.style.border = "medium dotted blue";
+   
 }
 var shipments = container.getElementsByClassName( "shipment")
 console.log( shipments.length )
@@ -21,6 +24,12 @@ function* orders( orderContainer ) {
     yield o.item(i)
   }
 }
+
+// Importan classes
+//
+// ordersContainer:	all elements of an order which can consist of several shipments
+// order-info:		core data, date, summ, order number, links to details
+//
 var doc = window.document
 var container = doc.getElementById( "ordersContainer")
 for (const o of orders( container )) {
@@ -89,6 +98,23 @@ var beschreibung = xpathItemsString( o, "./"+
 	"/div[@class='a-fixed-left-grid a-spacing-none']/div[@class='a-fixed-left-grid-inner']"+
 	"/div[@class='a-fixed-left-grid-col a-col-right']/div[@class='a-row']/a[@class='a-link-normal']"+
 	"")
+
+// div.a-box.shipment div.a-box-inner div.a-fixed-right-grid.a-spacing-top-medium div.a-fixed-right-grid-inner.a-grid-vertical-align.a-grid-top div.a-fixed-right-grid-col.a-col-left div.a-row div.a-fixed-left-grid.a-spacing-none div.a-fixed-left-grid-inner
+
+div.a-box.a-color-offset-background.order-info div.a-box-inner div.a-fixed-right-grid div.a-fixed-right-grid-inner
+div.a-box.a-color-offset-background.order-info div.a-box-inner div.a-fixed-right-grid div.a-fixed-right-grid-inner
+div.a-box.a-color-offset-background.order-info div.a-box-inner div.a-fixed-right-grid div.a-fixed-right-grid-inner
+div.a-box.a-color-offset-background.order-info div.a-box-inner div.a-fixed-right-grid div.a-fixed-right-grid-inner
+div.a-box-group:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > span:nth-child(2)
+
+// Datum
+div.a-fixed-right-grid-col.a-col-left div.a-row div.a-column.a-span4 div.a-row.a-size-base span.a-color-secondary.value
+// Summe
+div.a-fixed-right-grid-col.a-col-left div.a-row div.a-column.a-span2 div.a-row.a-size-base span.a-color-secondary.value
+// Bestellnummer
+div.a-fixed-right-grid-col.actions.a-col-right div.a-row.a-size-mini span.a-color-secondary.value
+// Details
+div.a-fixed-right-grid-col.actions.a-col-right div.a-row.a-size-base ul.a-unordered-list.a-nostyle.a-vertical a.a-link-normal
 
 var r = xpathItemsString( o, "./"+
 	"/div[@class='a-box shipment']/div[@class='a-box-inner']"+
